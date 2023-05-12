887e65a1b82ae30e85d3d8d123acd6bd7d760d24
Dima0687 <eis0687.dietmar@gmail.com>
2023-05-12 22:09:38 +0200

    splitted some code for readability and fixed bug where click on start while on fullscreen changed back to browser window

--- frontend/js/components/changeFullscreen.js
+++ frontend/js/components/changeFullscreen.js
@@ -1,118 +1,106 @@
 import { isGameRunning } from "./input.js";
+import { showMessageBox } from './messageBox.js';
 
 const toggleFullscreenBtn = document.querySelector("[data-fullscreen-toggle]");
 const gameBoardContainer = document.querySelector("[data-game-board-container]");
-const TIME_TILL_MSG_REMOVE_IN_MS = 5000;
 
-function toggleFullscreen() {
+
+export function toggleFullscreen( { clickOnStartBtn = false } = {} ) {
   let orientation = screen.orientation.type;
 
-  if(
-      (
-        document.fullscreenElement ||
-        document.mozFullScreenElement ||
-        document.msFullscreenElement ||
-        document.webkitFullscreenElement
-      ) && 
-      isGameRunning()
-  )
+  if( isFullscreenAndGameIsRunning() )
   {
     showMessageBox(
+      gameBoardContainer,
       "You shouldn't do that while the game is running!",
       "Anyway you can press escape or device defaults for leaving the fullscreen!"
     );
+
+    return 
   }
-  else if(
-      (
-        document.fullscreenElement ||
-        document.mozFullScreenElement ||
-        document.msFullscreenElement ||
-        document.webkitFullscreenElement
-      ) && 
-      !isGameRunning()
-  ) {
+  else if( isFullscreenAndGameIsNotRunning() ) {  
+    if(clickOnStartBtn) return;  
+    
     document.exitFullscreen();
     screen.orientation.unlock();
-    toggleFullscreenBtn.classList.toggle("fullscreen");
   }
   else {
-    gameBoardContainer.requestFullscreen();
 
-    toggleFullscreenBtn.classList.toggle("fullscreen");
+    gameBoardContainer.requestFullscreen();
     
-    if(
-      screen.orientation.type === "portrait-primary"   ||
-      screen.orientation.type === "landscape-secondary" 
-      ) {
-        screen.orientation.lock(orientation);
-      }
+    if( isMobile(orientation) ) {
+      screen.orientation.lock(orientation);
+    }
   }
 }
 
-function showMessageBox(...messages) {
-  createMessageBox(messages);
-}
-
-function createMessageBox(messages) {
-  const messageBox = document.createElement("div");
-  messageBox.className = "msg-box";
-
-  const triangle = document.createElement("span");
-  triangle.id = "triangle";
-
-  if(!messages.length) return;
-  messages = messages.map((msg) => {
-    if(msg === "") return;
-
-    const msgElem = document.createElement("p");
-    msgElem.textContent = msg;
-    
-    return msgElem;
-  });
-
-  messageBox.append(triangle, ...messages);
-
-  gameBoardContainer.append(messageBox);
-
-  removeMessageBox(messageBox);
-}
-
-function removeMessageBox(msgBox) {
-  if(!msgBox) return;
-
-  setTimeout(() => {
-    msgBox.remove();
-  }, TIME_TILL_MSG_REMOVE_IN_MS );
-}
 
 function toggleColorOfFullscreenBtn() {
-  if(isGameRunning() && document.fullscreenElement) {
+
+  if( isFullscreenAndGameIsRunning() ) {
+    // toggle fullscreen button is "color"
     toggleFullscreenBtn.classList.add("game-is-running");
     return;
   }
-
-  if(!document.fullscreenElement) {
-    toggleFullscreenBtn.classList.remove("fullscreen");
-  }
-
+  // toggle fullscreen button is standard "color"
   toggleFullscreenBtn.classList.remove("game-is-running");
 }
 
-export function toggleFullscreenClassOnStart() {
-  if(
-      !document.fullscreenElement ||
-      !document.mozFullScreenElement ||
-      !document.msFullscreenElement ||
-      !document.webkitFullscreenElement
-  ) {
-    gameBoardContainer.requestFullscreen();
-    toggleFullscreenBtn.classList.add("fullscreen");
+export function toggleFullscreenBtnClass() {
+  if( isNotInFullscreen() ) {
+    // compress icon is display: initial;
+    toggleFullscreenBtn.classList.remove("fullscreen");
+    return;
   }
+  // expand icon is display: initial;
+  toggleFullscreenBtn.classList.add("fullscreen");
 }
 
 export function draw() {
   toggleColorOfFullscreenBtn();
+  toggleFullscreenBtnClass();
 }
 
 
+function isFullscreenAndGameIsRunning() {
+  return (
+    (
+      document.fullscreenElement ||
+      document.mozFullScreenElement ||
+      document.msFullscreenElement ||
+      document.webkitFullscreenElement
+    ) && 
+    isGameRunning()
+  )
+}
+
+function isFullscreenAndGameIsNotRunning() {
+  return (
+    (
+      document.fullscreenElement ||
+      document.mozFullScreenElement ||
+      document.msFullscreenElement ||
+      document.webkitFullscreenElement
+    ) && 
+    !isGameRunning()
+  )
+}
+
+function isNotInFullscreen() {
+  return (
+    !document.fullscreenElement &&
+    !document.mozFullScreenElement &&
+    !document.msFullscreenElement &&
+    !document.webkitFullscreenElement
+  )
+}
+
+function isMobile(orientation) {
+  return (
+    orientation === "portrait-primary"   ||
+    orientation === "landscape-secondary" 
+  )
+}
+
 toggleFullscreenBtn.addEventListener("pointerup", toggleFullscreen);
+toggleFullscreenBtn.addEventListener("fullscreenchange", toggleFullscreenBtnClass);
